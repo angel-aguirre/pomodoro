8896fd519ad81de1f1b15f463896418a2fa3018c
Angel Aguirre <angelaguirrel@hotmail.com>
2022-05-26 19:59:41 -0500

    Se agregaron los colores de la app a la configuracion de Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
     content: [],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                main: '#30384B',
+                'main-light': '#3C4356',
+                'soft-red': '#FC4E4C',
+                'soft-green': '#05EB8D',
+            },
+        },
     },
     plugins: [],
     purge: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
