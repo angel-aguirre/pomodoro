bcc58d6c6c190aeacccbec2c1e793e6367bfbc00
Angel Aguirre <angelaguirrel@hotmail.com>
2022-11-26 18:35:11 -0600

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <template>
     <AppHeader />
-    <AppActions />
-    <AppTimer />
+    <div class="flex h-full flex-col">
+        <AppActions />
+        <AppTimer />
+    </div>
+    <AppFooter />
 </template>
 
 <script setup>
 import AppHeader from '@/components/layout/AppHeader.vue';
 import AppActions from '@/components/AppActions.vue';
 import AppTimer from '@/components/AppTimer.vue';
+import AppFooter from '@/components/layout/AppFooter.vue';
 </script>
 
 <style>
+html,
+body {
+    @apply h-full;
+}
+
 body {
     @apply bg-main;
     @apply text-white;
+}
+
+#app {
+    @apply h-full flex flex-col;
 }
 </style>
---
+++ src/components/layout/AppFooter.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer
+        class="relative container my-0 mx-auto font-inter text-xs pb-2 md:pb-8 flex justify-center items-center"
+    >
+        <div>Made with ♥ by Angel Aguirre &#169; {{ year }}</div>
+    </footer>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const year = computed(() => {
+    const _date = new Date();
+    return _date.getFullYear();
+});
+</script>
